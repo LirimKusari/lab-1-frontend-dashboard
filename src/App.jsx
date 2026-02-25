6d95f6579f3dc5f8c7bf148e5edf186f08726433
Oltix <ok58862@ubt-uni.net>
2026-02-25 02:26:38 +0100

    [DEV505] Created Sign in/ Sign up Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {Route , Routes} from 'react-router-dom'
 import {BrowserRouter} from 'react-router-dom'
@@ -6,12 +6,17 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+  {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/cart" element={<Cart/>}/>
